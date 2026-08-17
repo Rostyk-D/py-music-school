9f0ede8861af0dbd8fa9d130288d0a07310f7cc3
Ростислав Дурягін <rostyslav.duriahin@lnu.edu.ua>
2026-08-17 17:51:59 +0200

    Complete

--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,18 @@
-urlpatterns = []
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from musician.views import MusicianViewSet
+
+
+router = DefaultRouter()
+router.register(
+    "musician",
+    MusicianViewSet,
+    basename="manage"
+)
 
 app_name = "musician"
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
